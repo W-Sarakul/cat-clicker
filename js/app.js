50d6ce3454720d3da6e8069c9acdbb7170c827f6
Wassakorn Sarakul <w.m.sarakul@gmail.com>
2018-11-07 11:48:25 +0700

    Add the functionality for 2 cats

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-// get element
-let catPic = document.querySelector('#cat1');
+// get element for cat1
+let catPic1 = document.querySelector('#cat1');
 let cat1Clicks = document.querySelector('#cat1-click');
+
+// get element fir cat2
+let catPic2 = document.querySelector('#cat2');
+let cat2Clicks = document.querySelector('#cat2-click');
 
 // defind increment cat function
 function incrementCat() {
@@ -16,9 +20,15 @@ function incrementCat() {
 
 // Define the increment function
 let cat1In = incrementCat();
+let cat2In = incrementCat();
 
 
-catPic.addEventListener('click', function() {
+catPic1.addEventListener('click', function() {
   let clicks = cat1In();
   cat1Clicks.innerText = clicks;
 });
+
+catPic2.addEventListener('click', function() {
+  let clicks = cat2In();
+  cat2Clicks.innerText = clicks;
+})
