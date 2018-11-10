914282abaa4560602fe2c79f423df0de99cfdcc5
Wassakorn Sarakul <w.m.sarakul@gmail.com>
2018-11-10 12:49:03 +0700

    Add MVO form

--- js/app.js
+++ js/app.js
@@ -1,34 +1,75 @@
-// get element for cat1
-let catPic1 = document.querySelector('#cat1');
-let cat1Clicks = document.querySelector('#cat1-click');
+// // get element for cat1
+// let catPic1 = document.querySelector('#cat1');
+// let cat1Clicks = document.querySelector('#cat1-click');
+//
+// // get element fir cat2
+// let catPic2 = document.querySelector('#cat2');
+// let cat2Clicks = document.querySelector('#cat2-click');
+//
+// // defind increment cat function
+// function incrementCat() {
+//   let clicks = 0;
+//
+//   function increment() {
+//     clicks++;
+//     return clicks;
+//   }
+//
+//   return increment;
+// }
+//
+// // Define the increment function
+// let cat1In = incrementCat();
+// let cat2In = incrementCat();
+//
+//
+// catPic1.addEventListener('click', function() {
+//   let clicks = cat1In();
+//   cat1Clicks.innerText = clicks;
+// });
+//
+// catPic2.addEventListener('click', function() {
+//   let clicks = cat2In();
+//   cat2Clicks.innerText = clicks;
+// })
 
-// get element fir cat2
-let catPic2 = document.querySelector('#cat2');
-let cat2Clicks = document.querySelector('#cat2-click');
+$(function() {
+  var model = {
+    data: [{
+      name: 'Mang',
+      source: 'img/pic-1.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'Ruby',
+      source: 'img/pic-2.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'Tata',
+      source: 'img/pic-3.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'Judy',
+      source: 'img/pic-4.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'Koya',
+      source: 'img/puc-5.jpeg',
+      clicks: 0
+    }
+  ]
 
-// defind increment cat function
-function incrementCat() {
-  let clicks = 0;
+  };
 
-  function increment() {
-    clicks++;
-    return clicks;
-  }
+  var octopus = {
 
-  return increment;
-}
+  };
 
-// Define the increment function
-let cat1In = incrementCat();
-let cat2In = incrementCat();
+  var view = {
 
+  };
 
-catPic1.addEventListener('click', function() {
-  let clicks = cat1In();
-  cat1Clicks.innerText = clicks;
 });
-
-catPic2.addEventListener('click', function() {
-  let clicks = cat2In();
-  cat2Clicks.innerText = clicks;
-})
